78fbfa7b3edff7b3af8a38c00ffe0279b638ff10
zzh <zzh@cetccloud.com>
2021-01-08 12:42:35 +0800

    暂时注掉一些JDK9的语法,解决编译报错

--- Java8InAction-master/src/main/java/lambdasinaction/chap6/PartitionPrimeNumbers.java
+++ Java8InAction-master/src/main/java/lambdasinaction/chap6/PartitionPrimeNumbers.java
@@ -33,7 +33,9 @@ public class PartitionPrimeNumbers {
     public static boolean isPrime(List<Integer> primes, Integer candidate) {
         double candidateRoot = Math.sqrt((double) candidate);
         //return takeWhile(primes, i -> i <= candidateRoot).stream().noneMatch(i -> candidate % i == 0);
-        return primes.stream().takeWhile(i -> i <= candidateRoot).noneMatch(i -> candidate % i == 0);
+        //jdk9
+//        return primes.stream().takeWhile(i -> i <= candidateRoot).noneMatch(i -> candidate % i == 0);
+        return false;
     }
 /*
     public static <A> List<A> takeWhile(List<A> list, Predicate<A> p) {
